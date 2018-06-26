a60873a68a0485697f5e241b24684b332d0af943
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 03:07:40 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
+    line = "The line is currently:"
+    katz_deli.each_with_index {|name, index| line << " #{index+1}. #{name}" }
+    puts line
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli = katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  return katz_deli
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift()
+  end
+  return katz_deli
+end
