8403389ebe5d5bd870703d12699aff62ee29d5e7
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 02:59:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,11 @@
 katz_deli = []
 
 def line(katz_deli)
-  
+  if katz_deli.length == 0
+    return "The line is currently empty."
+  else
+    line = "The line is currently:"
+    katz_deli.each_with_index {|name, index| " #{index+1}. #{name}" }
 end
 
 def take_a_number(katz_deli, name)
